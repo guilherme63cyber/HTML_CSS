f3c00dd2e6f31466a7a9a2adb0b69b9e537ff68f
guilherme63cyber <guilhermediasoliveira63@gmail.com>
2024-10-31 15:59:21 -0300

    first commit

---
+++ PROJETO_VIAGEM/script.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector('#header')
+
+document.addEventListener("scroll",()=> {
+  let scrolltop = window.scrollY
+
+   if (scrolltop > 0) {
+   navbar.classList.add('rolar')
+   }else{
+   navbar.classList.remove('rolar')
+} 
+})
+
